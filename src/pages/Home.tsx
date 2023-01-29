4a1be80c6b223f1f3b53e2f7f0f1051a8f7abdaf
Guilherme Fontebasso <guitavaresgames@gmail.com>
2023-01-29 17:10:01 -0300

    Changes: fixed some styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import { ExternalLinks } from '../components/Link/ExternalLinks';
 export const Home = () => {
   return (
     <section className="w-full flex justify-center items-center flex-col py-10 sm:px-60 sm:py-20 md:px-40">
-      <section className="flex items-center justify-center flex-col pb-20 sm:pb-10">
+      <section className="flex items-center justify-center flex-col pb-20 sm:pb-10 2xl:pb-32">
         <Title
           text="Rick and Morty I.N.F.O"
           className="text-center text-4xl font-bold p-8 sm:text-6xl"
